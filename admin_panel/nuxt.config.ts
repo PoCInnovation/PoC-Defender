ec9f9d483aee2cb8320b642842ec0267a677163e
maitrecraft1234 <victor.jurgens-mestre@epitech.eu>
2025-05-26 16:42:02 +0200

    chore: attempted fix by updating the packages
    css issues still onworking

--- admin_panel/nuxt.config.ts
+++ admin_panel/nuxt.config.ts
@@ -1,4 +1,10 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
+    css: ['~/assets/css/tailwind.css'],
+    postcss: {
+        plugins: {
+            autoprefixer: {},
+        },
+    },
 })
